6766ecbbefb0bce1fd0237fc1e9e552a4a3f77d9
chou12727 <chou12727@yahoo.co.jp>
2025-05-21 11:03:14 +0900

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const target = process.env.VITE_API_URL|| 'http://localhost:4000'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: target,
         changeOrigin: true
       }
     }
