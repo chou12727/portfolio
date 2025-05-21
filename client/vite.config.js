4ddcb0a9a6180629c1f5ccaf7c90d08acaa22174
chou12727 <chou12727@yahoo.co.jp>
2025-05-21 11:08:53 +0900

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ const target = process.env.VITE_API_URL|| 'http://localhost:4000'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, 
+  },
   server: {
     proxy: {
       '/api': {
